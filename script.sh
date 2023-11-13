771345b2a3b48bb266030e2cc54f0f4e62a62011
Berian James <1518788+berianjames@users.noreply.github.com>
2023-11-13 13:50:16 +0100

    karolina batch job config

---
+++ script.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+#SBATCH --job-name HyperMagnetics
+#SBATCH --account DD-23-133
+#SBATCH --partition qgpu
+#SBATCH --nodes 1
+#SBATCH --ntasks-per-node 128
+#SBATCH --time 12:00:00
+#SBATCH --gpus 1
+
+conda activate hypermagnetics
+wandb login
+python src/hypermagnetics/trainer.py
