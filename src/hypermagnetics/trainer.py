9a4af8f3dcaa854ceefc2b5d9bc25d856abeb9ba
Berian James <1518788+berianjames@users.noreply.github.com>
2023-11-13 09:50:30 +0100

    wandb parameter sweep

--- src/hypermagnetics/trainer.py
+++ src/hypermagnetics/trainer.py
@@ -8,6 +8,18 @@ import hypermagnetics.sources as sources
 import wandb
 from hypermagnetics.models import HyperMLP
 
+sweep_configuration = {
+    "method": "grid",
+    "name": "grid-search",
+    "parameters": {
+        "width": {"values": [16, 32, 64]},
+        "depth": {"values": [2, 3, 4]},
+        "hdepth": {"values": [1, 2, 3]},
+    },
+}
+
+sweep_id = wandb.sweep(sweep=sweep_configuration, project="hypermagnetics")
+
 
 def loss(model, sources, grid, target):
     pred = jax.vmap(model, in_axes=(0, None))(sources, grid)
@@ -15,11 +27,11 @@ def loss(model, sources, grid, target):
 
 
 @eqx.filter_jit
-def step(model, opt_state, sources, grid, target):
+def step(model, optim, opt_state, sources, grid, target):
     loss_value, grads = eqx.filter_value_and_grad(loss)(model, sources, grid, target)
     updates, opt_state = optim.update(grads, opt_state, model)
     model = eqx.apply_updates(model, updates)
-    return model, opt_state, loss_value
+    return model, optim, opt_state, loss_value
 
 
 @eqx.filter_jit
@@ -30,35 +42,32 @@ def accuracy(model, data):
     return jnp.median(diff / jnp.linalg.norm(target) * 100)
 
 
-if __name__ == "__main__":
+def main():
+    run = wandb.init()
+    width = wandb.config.width
+    depth = wandb.config.depth
+    hdepth = wandb.config.hdepth
+
     source_config = {"N": 500, "M": 3, "lim": 3, "res": 32}
     train = sources.configure(**source_config, key=jr.PRNGKey(40))
     val = sources.configure(**source_config, key=jr.PRNGKey(41))
 
     hyperkey, mainkey = jr.split(jr.PRNGKey(42), 2)
     model_config = {
-        "width": 32,
-        "depth": 3,
-        "hdepth": 2,
+        "width": width,
+        "depth": depth,
+        "hdepth": hdepth,
     }
     model = HyperMLP(**model_config, hyperkey=hyperkey, mainkey=mainkey)
 
-    trainer_config = {"learning_rate": 0.001, "epochs": 1000}
+    trainer_config = {"learning_rate": 0.001, "epochs": 10000}
     optim = optax.adam(trainer_config["learning_rate"])
     opt_state = optim.init(eqx.filter(model, eqx.is_array))
 
-    run = wandb.init(
-        project="hypermagnetics",
-        config={
-            "trainer_config": trainer_config,
-            "model_config": model_config,
-            "source_config": source_config,
-        },
-    )
-
     for epoch in range(trainer_config["epochs"]):
-        model, opt_state, train_loss = step(
+        model, optim, opt_state, train_loss = step(
             model,
+            optim,
             opt_state,
             train["sources"],
             train["grid"],
@@ -67,11 +76,15 @@ if __name__ == "__main__":
         train_acc = accuracy(model, train)
         val_acc = accuracy(model, val)
 
-        # if (epoch % (trainer_config["epochs"] / 100)) == 0:
         wandb.log(
             {
+                "epoch": epoch,
                 "train_loss": train_loss,
                 "train_acc": train_acc,
                 "val_acc": val_acc,
             }
         )
+
+
+# Start sweep job.
+wandb.agent(sweep_id, function=main)
