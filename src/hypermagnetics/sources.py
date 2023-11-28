5a09bb925013f64160dff2552afef31b8b76546d
Berian James <1518788+berianjames@users.noreply.github.com>
2023-11-28 16:27:20 +0100

    include plotting in sources module

--- src/hypermagnetics/sources.py
+++ src/hypermagnetics/sources.py
@@ -1,6 +1,8 @@
 import jax
 import jax.numpy as jnp
 import jax.random as jr
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 def _potential(m, r0, r):
@@ -56,6 +58,71 @@ def configure(n_samples, n_sources, lim=3, res=32, key=jr.PRNGKey(0)):
     }
 
 
+def plots(sources, idx=0, model=None, show_field=False):
+    """Plots the sources and field/potential of a single sample."""
+    mr = sources["sources"]
+    m, r0 = mr[:, :, 0:1], mr[:, :, 2:3]
+    grid = sources["grid"]
+
+    res = int(jnp.sqrt(len(grid)))
+    N = len(mr)
+
+    x_grid = np.array(grid[:, 0].reshape((res, res)))
+    y_grid = np.array(grid[:, 1].reshape((res, res)))
+    potential = sources["potential"].reshape((N, res, res))
+    field = sources["field"].reshape((N, res, res, 2))
+
+    _, axes = plt.subplots(1, 2, figsize=(12, 6))
+
+    # Subplot 1: Magnetic Scalar Potential
+    _ = axes[0].contourf(x_grid, y_grid, potential[idx])
+    # plt.colorbar(cp, ax=axes[0])
+    axes[0].scatter(r0[idx, :, 0], r0[idx, :, 1], color="red")
+    axes[0].quiver(
+        r0[idx, :, 0],
+        r0[idx, :, 1],
+        m[idx, :, 0],
+        m[idx, :, 1],
+        angles="xy",
+        scale_units="xy",
+        scale=1,
+        color="red",
+    )
+    axes[0].set_title("Magnetic Scalar Potential")
+    axes[0].set_xlabel("x")
+    axes[0].set_ylabel("y")
+
+    # Subplot 2: Magnetic Field
+    if show_field is not False:
+        axes[1].streamplot(
+            x_grid,
+            y_grid,
+            field[idx, :, :, 0],
+            field[idx, :, :, 1],
+            density=1.5,
+            linewidth=1,
+            arrowsize=1.5,
+            arrowstyle="->",
+        )
+        axes[1].scatter(r0[idx, :, 0], r0[idx, :, 1], color="red")
+        axes[1].quiver(
+            r0[idx, :, 0],
+            r0[idx, :, 1],
+            m[idx, :, 0],
+            m[idx, :, 1],
+            angles="xy",
+            scale_units="xy",
+            scale=1,
+            color="red",
+        )
+        axes[1].set_title("Magnetic Field")
+        axes[1].set_xlabel("x")
+        axes[1].set_ylabel("y")
+
+    plt.tight_layout()
+    plt.show()
+
+
 def sample_grid(key, lim, res, n=None):
     if n is None:
         n = res * res
@@ -65,10 +132,11 @@ def sample_grid(key, lim, res, n=None):
 if __name__ == "__main__":
     config = {
         "n_samples": 10,
-        "n_sources": 1,
+        "n_sources": 2,
         "key": jr.PRNGKey(40),
         "lim": 3,
         "res": 32,
     }
     train_data = configure(**config)
     print(train_data["potential"].shape, train_data["field"].shape)
+    plots(train_data, idx=0, show_field=True)
