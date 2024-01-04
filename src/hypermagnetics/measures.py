1cb309edca1d7fd5bfe6fe10360a4f06f6291a7d
Berian James <1518788+berianjames@users.noreply.github.com>
2024-01-04 12:02:14 +0100

    docstrings for loss and error measures

--- src/hypermagnetics/measures.py
+++ src/hypermagnetics/measures.py
@@ -6,6 +6,16 @@ import optax
 
 @eqx.filter_jit
 def loss(model, data):
+    """
+    Calculates the loss function for the given model and data.
+
+    Parameters:
+    - model (callable): The model function that takes in sources and grid as inputs and returns predictions.
+    - data (dict): A dictionary containing the input data, including sources, grid, and target potential.
+
+    Returns:
+    - The mean loss value calculated using the Huber loss function.
+    """
     sources, grid, target = data["sources"], data["grid"], data["potential"]
     pred = jax.vmap(model, in_axes=(0, None))(sources, grid)
     return jnp.mean(optax.huber_loss(pred, target))
@@ -13,6 +23,17 @@ def loss(model, data):
 
 @eqx.filter_jit
 def accuracy(model, data):
+    """
+    Calculate the median relative error of the model given the data.
+
+    Parameters:
+    - model (callable): The model function that takes in sources and grid as inputs and returns predictions.
+    - data (dict): A dictionary containing the input data, including sources, grid, and target potential.
+
+    Returns:
+    float: The median relative error of the model, as a percentage.
+
+    """
     sources, grid, target = data["sources"], data["grid"], data["potential"]
     pred = jax.vmap(model, in_axes=(0, None))(sources, grid)
     diff = jnp.linalg.norm(target - pred)
