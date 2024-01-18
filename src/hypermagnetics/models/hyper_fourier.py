d2e94aae643020cc0f59ed064203a5d766f092c1
Berian James <1518788+berianjames@users.noreply.github.com>
2024-01-18 10:28:42 +0100

    include fourier model bias through zero mode

--- src/hypermagnetics/models/hyper_fourier.py
+++ src/hypermagnetics/models/hyper_fourier.py
@@ -18,9 +18,9 @@ def evaluate_basis(omega, r):
     return jnp.stack(
         [
             basis_term(jnp.cos, jnp.cos, omega, r),
-            basis_term(jnp.sin, jnp.sin, omega, r),
-            basis_term(jnp.cos, jnp.sin, omega, r),
             basis_term(jnp.sin, jnp.cos, omega, r),
+            basis_term(jnp.cos, jnp.sin, omega, r),
+            basis_term(jnp.sin, jnp.sin, omega, r),
         ],
         axis=0,
     )
@@ -28,15 +28,12 @@ def evaluate_basis(omega, r):
 
 class FourierHyperModel(eqx.Module):
     w: eqx.nn.MLP
-    b: eqx.nn.Linear
 
     def __init__(self, out: int, width: int, depth: int, key: jr.PRNGKeyArray):
-        weightkey, biaskey = jr.split(key, 2)
-        self.w = eqx.nn.MLP(4, out, width, depth, jax.nn.gelu, key=weightkey)
-        self.b = eqx.nn.Linear(4, "scalar", key=biaskey)
+        self.w = eqx.nn.MLP(4, out, width, depth, jax.nn.gelu, key=key)
 
     def __call__(self, sources):
-        return self.w(sources), self.b(sources)
+        return self.w(sources)
 
 
 class FourierModel(HyperModel):
@@ -57,23 +54,26 @@ class FourierModel(HyperModel):
 
     @property
     def omega(self):
-        modes = jnp.floor(jnp.logspace(0, self.lfmax - self.lfmin, self.order))
-        return jnp.squeeze(2 * jnp.pi * modes * 10**self.lfmin)
+        modes = jnp.squeeze(
+            jnp.floor(jnp.logspace(0, self.lfmax - self.lfmin, self.order - 1))
+        )
+        modes = jnp.concatenate((jnp.zeros(1), modes))  # Append zero mode
+        return 2 * jnp.pi * modes * 10**self.lfmin
 
     @eqx.filter_jit
-    def fourier_expansion(self, weights, bias, r):
+    def fourier_expansion(self, weights, r):
         weights = jnp.reshape(weights, (4, self.order, self.order))
         basis_terms = evaluate_basis(self.omega, r)
         elementwise_product = weights * basis_terms
         summed_product = jnp.sum(elementwise_product, axis=(0, 1, 2))
-        return bias + summed_product
+        return summed_product
 
     def prepare_weights(self, sources):
-        w, b = jax.vmap(self.hypermodel)(sources)
-        return jnp.sum(w, axis=0), jnp.sum(b, axis=0)
+        w = jax.vmap(self.hypermodel)(sources)
+        return jnp.sum(w, axis=0), None
 
     def prepare_model(self, weights, bias):
-        return lambda r: self.fourier_expansion(weights, bias, r)
+        return lambda r: self.fourier_expansion(weights, r)
 
 
 if __name__ == "__main__":
