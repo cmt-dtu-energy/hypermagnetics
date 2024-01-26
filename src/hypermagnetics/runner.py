173d36eb30d7196298efc827508f324f95bd4db6
Berian James <1518788+berianjames@users.noreply.github.com>
2024-01-26 14:03:12 +0100

    tweak plots; compute single and multi-source error

--- src/hypermagnetics/runner.py
+++ src/hypermagnetics/runner.py
@@ -1,5 +1,7 @@
 import equinox as eqx
+import matplotlib.pyplot as plt
 import optax
+import scienceplots  # noqa
 import yaml
 
 import hypermagnetics.sources as sources
@@ -9,8 +11,10 @@ from hypermagnetics.measures import accuracy, loss
 from hypermagnetics.models.hyper_fourier import FourierModel  # noqa
 from hypermagnetics.models.hyper_mlp import HyperLayer, HyperMLP  # noqa
 
+plt.style.use(["science", "ieee"])
 
-def fit(trainer_config, optim, model, train, val, log=print, every=1):
+
+def fit(trainer_config, optim, model, train, test, log=print, every=1):
     opt_state = optim.init(eqx.filter(model, eqx.is_array))
 
     @eqx.filter_jit
@@ -23,17 +27,15 @@ def fit(trainer_config, optim, model, train, val, log=print, every=1):
     for epoch in range(trainer_config["epochs"]):
         model, opt_state, train_loss = step(model, opt_state, train)
         train_err = accuracy(model, train)
-        val_err = accuracy(model, val)
+        test_err = accuracy(model, test)
         log(
             {
                 "epoch": epoch,
                 "train_loss": train_loss.item(),
                 "train_err": train_err.item(),
-                "val_err": val_err.item(),
+                "test_err": test_err.item(),
             }
         ) if (epoch % every == 0) else None
-        if train_err < 1.9:
-            break
 
     return model
 
@@ -45,12 +47,13 @@ if __name__ == "__main__":
     source_config = run_configuration["sources"]
     train = sources.configure(**source_config["train"])
     test = sources.configure(**source_config["test"])
-    val = sources.configure(**source_config["val"])
+    val_single = sources.configure(**source_config["val-single"])
+    val_multi = sources.configure(**source_config["val-multi"])
 
     model_config = run_configuration["model"]
     # model = FourierModel(**model_config["fourier"])
-    # model = HyperLayer(**model_config["hyperlayer"])
-    model = HyperMLP(**model_config["hypernetwork"])
+    model = HyperLayer(**model_config["hyperlayer"])
+    # model = HyperMLP(**model_config["hypernetwork"])
 
     schedule = run_configuration["schedule"]
     wandb.init(
@@ -65,8 +68,19 @@ if __name__ == "__main__":
         model = fit(trainer_config, optim, model, train, test, log=wandb.log, every=10)
         # wandb.log({"mode_limits": model.kl})
 
+    train_err = accuracy(model, train)
+    val_single_err = accuracy(model, val_single)
+    val_multi_err = accuracy(model, val_multi)
+    wandb.log(
+        {
+            "train_err": train_err.item(),
+            "val_single_err": val_single_err.item(),
+            "val_multi_err": val_multi_err.item(),
+        }
+    )
+
     # save(model, wandb.run.id)
     plots(train, model, idx=0, output="wandb")
-    plots(test, model, idx=0, output="wandb")
-    plots(val, model, idx=0, output="wandb")
+    plots(val_single, model, idx=0, output="wandb")
+    plots(val_multi, model, idx=0, output="wandb")
     wandb.finish()
