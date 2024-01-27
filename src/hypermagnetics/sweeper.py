f0a7d458dda9e575d1612530201c316a151493e0
Berian James <1518788+berianjames@users.noreply.github.com>
2024-01-27 15:47:32 +0100

    trial sweep config

--- src/hypermagnetics/sweeper.py
+++ src/hypermagnetics/sweeper.py
@@ -1,11 +1,17 @@
-import jax.random as jr
+import matplotlib.pyplot as plt
 import optax
+import scienceplots  # noqa
 import yaml
 
 import hypermagnetics.sources as sources
 import wandb
-from hypermagnetics.models import HyperMLP
+from hypermagnetics import plots
+from hypermagnetics.measures import accuracy
+from hypermagnetics.models.hyper_fourier import FourierModel  # noqa
+from hypermagnetics.models.hyper_mlp import HyperLayer, HyperMLP  # noqa
 from hypermagnetics.runner import fit
+
+plt.style.use(["science", "ieee"])
 
 with open("config/sweep-configuration.yaml", "r") as f:
     sweep_configuration = yaml.safe_load(f)
@@ -18,20 +24,38 @@ sweep_id = wandb.sweep(
 def main():
     wandb.init()
 
-    source_config = wandb.config.source
-    train = sources.configure(**source_config, key=jr.PRNGKey(40))
-    val = sources.configure(**source_config, key=jr.PRNGKey(41))
+    source_config = sweep_configuration["sources"]
+    train = sources.configure(**source_config["train"])
+    test = sources.configure(**source_config["test"])
+    val_single = sources.configure(**source_config["val-single"])
+    val_multi = sources.configure(**source_config["val-multi"])
 
-    key = jr.PRNGKey(42)
-    model_config = wandb.config.model
-    model = HyperMLP(**model_config, key=key)
+    model_config = sweep_configuration["model"]
+    # model = FourierModel(**model_config["fourier"])
+    model = HyperLayer(**model_config["hyperlayer"])
+    # model = HyperMLP(**model_config["hypernetwork"])
+    wandb.log({"nparams": model.nparams})
 
-    trainer_config = wandb.config.trainer
-    learning_rate = 10 ** trainer_config["log_learning_rate"]
+    schedule = sweep_configuration["schedule"]
+    for trainer_config in schedule:
+        optim = optax.adam(**trainer_config["params"])
+        model = fit(trainer_config, optim, model, train, test, log=wandb.log, every=10)
 
-    wandb.log({"nparams": model.nparams, "learning_rate": learning_rate})
-    optim = optax.adam(learning_rate=learning_rate, b1=0.99)
-    fit(trainer_config, optim, model, train, val, log=wandb.log)
+    train_err = accuracy(model, train)
+    val_single_err = accuracy(model, val_single)
+    val_multi_err = accuracy(model, val_multi)
+
+    wandb.log(
+        {
+            "train_err": train_err.item(),
+            "val_single_err": val_single_err.item(),
+            "val_multi_err": val_multi_err.item(),
+        }
+    )
+
+    plots(train, model, idx=0, output="wandb")
+    plots(val_single, model, idx=0, output="wandb")
+    plots(val_multi, model, idx=0, output="wandb")
     wandb.finish()
 
 
